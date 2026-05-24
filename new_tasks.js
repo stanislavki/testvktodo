5c1c64ddf9e400de9a12bc17a8187eb1cbffdc00
stanislavki <naustas2007qwe@gmail.com>
2026-05-24 18:00:18 +0500

    Fix API_URL by removing trailing slash

--- new_tasks.js
+++ new_tasks.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://todo-stasnau.amvera.io/';
+const API_URL = 'https://todo-stasnau.amvera.io';
 
         document.addEventListener('DOMContentLoaded', async () => {
             const userId = localStorage.getItem('user_id');
